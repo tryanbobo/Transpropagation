159a184435fbd4b4675e766fe171f50688bd2de1
Ryan Bobo <tb1302@txstate.edu>
2021-04-20 12:24:19 -0500

    update from home

--- Bobo_GEO5419Final.py
+++ Bobo_GEO5419Final.py
@@ -26,10 +26,26 @@ def eucDistance():
     apFc = path + "\RADIO_POINTS_TXST.shp"
     rast = path + r"\dem_quad_50cm\dem_quad_50cm.tif"
     studyArea = "\StudyArea.shp"
-    Gt = 28
-    Gr = 3
-    Pt = 2.2
-    lambduh = .0559
+    ############################Radio Variables############################
+    #specs for ruckus T610(used at TXST): Gt = 6dBm, Pt = 28dBm
+    #Frequency
+        #ISM (2.4-2.484GHz)
+        #U-NII-1 (5.15-5.25GHz)
+        #U-NII-2A (5.25-5.35GHz)
+        #U-NII-2C (5.47-5.725GHz)
+        #U-NII-3 (5.725-5.85GHz)
+    #Gain:
+        # Up to 6dBm
+    Gt = 6 #0.0079432823472#6 #Watts or 6 dBm #Transmitter Gain () convert from decibels to a power ratio
+    Gr = 2 #0.0031628 #Watts#2-5 dBd common with mobile phones#Receiver Gain () convert from decibels to a power ratio
+    Pt = 28# 1 Watts or 28dBm (Transmitter Power (Watts))
+    #Pr = to value we are solving for is measured in Watts
+    c = 299792458 # speed of light in m/s
+    f = 2400000000
+    lambduh = 0.1249 #c/f # = c/f (c=speed of light in m/s & f=frequency in Hz)
+                    # c = 300,000,000 m/s
+    outFriis = 28 + 6 + 2 + 20 * Log10((.1248 / 4 * 3.14 * 40))
+    print(outFriis)
 
     FIDnum = []
     #cursor = arcpy.SearchCursor(apFc, ["FID"])
@@ -51,25 +67,25 @@ def eucDistance():
         #how do you make units global for multiple users???
         outEucDistance = EucDistance("indLyr%s"%FID, cell_size = rast)
         print("Created Euclidean Distance of layer %s" % FID)
-        #outEucDistance.save('dist_%s' %FID)
+        outEucDistance.save('dist_%s' %FID)
 
         #apply friis ect. transmission equations...
         #if user input picking different eqs
         # Example Friis EQ: 28 * 3 * 2.2 *  Square(.0559 / (4 * 3.14 *  Raster("distMint")))
 
-        outFriis = Gt * Gr * Pt * Square(lambduh / 4 * math.pi * outEucDistance)
+        #outFriis = Gt * Gr * Pt * Square(lambduh / 4 * math.pi * (outEucDistance * .3048))
+        outFriis = Gt + Gr + Pt + (20 *Log10((lambduh / 4 * 3.14 * (outEucDistance * .3048))))
         print("Created Friis calculation of layer %s" % FID)
-        #outFriis.save("friisOut%s" %FID)
+        outFriis.save("friisOut%s.tif" %FID)
         # Convert WATTS to dBm: 10 * Log10(1000*"rssW")
-        convertdBm = 10 * Log10(1000*outFriis)
+        #convertdBm = 10 * Log10(1000*outFriis)
         #convertdBm.save("friisOutdBm%s" %FID)
-        print("Converted Watts to dBm for layer %s" % FID)
+        #print("Converted Watts to dBm for layer %s" % FID)
 
-        viewShed = Viewshed(rast, "indLyr%s"%FID)
+        #viewShed = Viewshed(rast, "indLyr%s"%FID)
         #viewShed.save("viewShedOut%s" %FID)
 
-        friisView = viewShed * convertdBm
-        friisView.save("friisView%s" %FID)
+        #friisView = viewShed * convertdBm
+        #friisView.save("friisView%s.tif" %FID)
+
 eucDistance()
-
-
